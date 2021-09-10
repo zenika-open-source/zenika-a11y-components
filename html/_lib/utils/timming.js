f6e6f1d38ac68bb7a8e85588b30fd7d1a760aa1f
Jérémie Patonnier <jpatonnier@zenika.com>
2021-09-10 15:17:44 +0200

    Create timming utils

--- html/_lib/utils/timming.js
+++ html/_lib/utils/timming.js
@@ -1,4 +1,26 @@
 /**
+ * A simple Promise that wait for document readyness.
+ *
+ * Because JS module are defered, we need to be carefule and wait for
+ * `DOMContentLoaded` to be fired before accessing the DOM.
+ */
+export const ready = new Promise((resolve) => {
+  function isReady() {
+    window.removeEventListener('DOMContentLoaded', isReady)
+    resolve()
+  }
+
+  window.addEventListener('DOMContentLoaded', isReady)
+
+  if (
+    document.readyState === 'interactive'
+    || document.readyState === 'complete'
+  ) {
+    isReady()
+  }
+})
+
+/**
  * Wait for a full run of the event loop
  *
  * Some browser, like Safari, can require a full run of the event loop to
